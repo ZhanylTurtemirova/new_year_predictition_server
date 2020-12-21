7d8aa3492d654976adef8866969c22acf80a6f2f
zhanyl <turtemirova.zhanyl@gmail.com>
2020-12-21 16:49:38 +0600

    app.js changes

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
 
   // Handle React routing, return all requests to React app
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "/client/build/index.html"));
-  });
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  // });
 }
 
 // if (process.env.NODE_ENV && process.env.NODE_ENV === "production") {
