2ba89e5b7e8814b3081118fb8f91ab6095445c7c
zhanyl <turtemirova.zhanyl@gmail.com>
2020-12-21 10:15:31 +0600

    change add.js

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ const express = require("express");
 require("dotenv").config();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGOURI = process.env.MONGOURI;
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(_dirname, "client", "build", "index.html"));
+  });
+}
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
