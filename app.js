942114bf28fcb1f1276937919318a31776f09d89
zhanyl <turtemirova.zhanyl@gmail.com>
2020-12-21 13:42:28 +0600

    change app.js

--- app.js
+++ app.js
@@ -7,12 +7,22 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGOURI = process.env.MONGOURI;
-if (process.env.NODE_ENV && process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(_dirname, "client", "build", "index.html"));
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  // Handle React routing, return all requests to React app
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
+
+// if (process.env.NODE_ENV && process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(_dirname, "client", "build", "index.html"));
+//   });
+// }
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
