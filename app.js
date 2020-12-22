202173e24d7ab86f154593c1a947e412c8d35683
zhanyl <turtemirova.zhanyl@gmail.com>
2020-12-22 14:19:49 +0600

    design fixes

--- app.js
+++ app.js
@@ -10,19 +10,7 @@ const MONGOURI = process.env.MONGOURI;
 if (process.env.NODE_ENV === "production") {
   // Serve any static files
   app.use(express.static(path.join(__dirname, "client/build")));
-
-  // Handle React routing, return all requests to React app
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.join(__dirname + "/client/build/index.html"));
-  // });
 }
-
-// if (process.env.NODE_ENV && process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(_dirname, "client", "build", "index.html"));
-//   });
-// }
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
